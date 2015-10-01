3be00b561b9603910d44185b0b2852d57b564238
mapleez <kukunanhai_5207@126.com>
2015-10-01 15:55:19 +0800

    all the merge.

---
+++ src/ez_display.c
@@ -0,0 +1,44 @@
+#include "ez_display.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void
+ez_disp_file_simple (pfile_entity _f) {
+
+}
+
+void
+ez_disp_eat_info (pfile_entity _f) {
+
+}
+
+void
+ez_disp_check (pfile_entity _f) {
+	if (! _f) {
+		
+	}
+}
+
+void
+ez_disp_eat_info (pfile_entity _f) {
+}
+
+void
+ez_disp_iat_info (pfile_entity _f) {
+
+}
+
+void
+ez_disp_arch (pfile_entity _f) {
+}
+
+void
+ez_disp_fields (pfile_entity _f) {
+}
+
+void
+ez_disp_help () {
+}
+
+
