e7dc95e5ea2e2509a877d1289b6ef34284052766
Wave-Maker <252689563@qq.com>
2015-10-02 16:44:51 +0800

    canshu

--- include/hh_fengine.h
+++ include/hh_fengine.h
@@ -2,8 +2,8 @@
 #  define _FENGINE_H_
 #include ez_file_entity.h
 void init(char *filename);
-int read_dos_head();
-int read_NT_head();
-int read_segment_header();
-int desponse();
+int read_dos_head(pfile_entity dosHeader);
+int read_NT_head(pfile_entity ntHeader);
+int read_segment_header(pfile_entity segHeader);
+int desponse(pfile_entity free);
 #endif //~ _FENGINE_H_
